49b510284cdf1016dfc344a4f5f7d55bf7977977
Youngtechie <olaegbeolamilekan1@gmail.com>
2024-12-16 13:11:45 +0100

    [UI]:: fixes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -12,7 +12,7 @@ import {
 
 const AboutPage = () => {
   return (
-    <div className="text-white lg:bg-[#151615] flex flex-col w-full items-center space-y-6 pt-[90px] lg:mt-0 p-3 lg:p-6 pb-[100px] min-h-screen lg:h-full overflow-y-auto">
+    <div className="text-white bg-[#151615] flex flex-col w-full items-center space-y-6 pt-[90px] lg:mt-0 p-3 lg:p-6 pb-[100px] min-h-screen lg:h-full overflow-y-auto">
       <div>
         <motion.div
           initial={{ opacity: 0, y: 50 }}
